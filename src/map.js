ba4343e0b7f09164cdf5b81e679a269b4dafa79b
Jasmine <jasmineoliver158@gmail.com>
2026-04-10 13:55:48 -0400

    search functionality

--- src/map.js
+++ src/map.js
@@ -1,7 +1,9 @@
 let marker;
-
-
+const form = document.querySelector('form');
 const map = L.map('map').setView([51.505, -0.09], 13);
+
+form.addEventListener("submit", handleSearch);
+
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
@@ -10,12 +12,36 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 
 
-fetchIP().then(data => {
+async function init() {
+    const data = await fetchIP();
+
     const lat = data.location.lat;
     const lng = data.location.lng;
 
-    map.setView([lat,lng], 13);
+    map.setView([lat, lng], 13);
 
-    marker = L.marker([lat,lng]).addTo(map);
+    marker = L.marker([lat, lng]).addTo(map);
+}
 
-});
+init();
+
+async function handleSearch(e) {
+    // console.log("submit working");
+    e.preventDefault();
+
+    const inputIP = document.getElementById('searchInput').value;
+    console.log("input value", inputIP);
+    const data = await fetchIP(inputIP);
+
+    const lat = data.location.lat;
+    const lng = data.location.lng;
+
+    map.setView([lat, lng], 13);
+
+    marker.setLatLng([lat, lng]);
+
+    form.reset();
+
+}
+
+
