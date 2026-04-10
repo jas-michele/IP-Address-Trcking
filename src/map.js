d72e88b8b103ac8c02ca115a469dd623635e8b87
Jasmine <jasmineoliver158@gmail.com>
2026-04-10 11:43:41 -0400

    api returning data

--- src/map.js
+++ src/map.js
@@ -1,3 +1,5 @@
+let marker;
+
 
 const map = L.map('map').setView([51.505, -0.09], 13);
 
@@ -5,3 +7,15 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+
+
+fetchIP().then(data => {
+    const lat = data.location.lat;
+    const lng = data.location.lng;
+
+    map.setView([lat,lng], 13);
+
+    marker = L.marker([lat,lng]).addTo(map);
+
+});
